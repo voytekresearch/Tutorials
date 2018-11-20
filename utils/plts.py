e0e37674727f5c0c994f74eac7dc421a9c1aabb6
Julio Dominguez <jcdoming@ucsd.edu>
2018-11-20 14:49:33 -0800

    removed utils/noise.py

--- utils/plts.py
+++ utils/plts.py
@@ -28,3 +28,38 @@ def plot_fm_shading(fm, cens, bws):
     for cen, bw in zip(cens, bws):
         plt.axvspan(cen, cen, color='g')
         plt.axvspan(cen-(m*bw), cen+(m*bw), color='r', alpha=0.2, lw=0)
+
+    
+def noise_time_plot(data, time_scale):
+
+    #plot 1 second of pink noise data
+    plt.figure(figsize = [12,6])
+    plt.plot(time_scale[0:1000], data[0:1000]) #plot samples over time
+    
+def noise_frequency_plot(fourier, fx_bins, title):
+    # calculating fourier transform
+    # we're going to take a sample of the data to keep fx bins at a reasonable size.
+   
+    plt.figure(figsize=(16,10))
+    plt.subplot(1,2,1)
+    plt.plot(fx_bins[0:1500],abs(fourier[0:1500])) 
+    plt.ylabel('Power')
+    plt.xlabel('Frequency (Hz)')
+    plt.title(title)
+
+    #same thing but in log space
+    plt.subplot(1,2,2)
+    plt.plot(fx_bins[0:1500],np.log(abs(fourier[0:1500]))) 
+    plt.ylabel('log Power')
+    plt.xlabel('Frequency (Hz)')
+    plt.title(title) 
+    
+def welch_plot(fs, ps, title):
+    #Welch's PSD of brown noise
+   
+    plt.figure(figsize=(16,10))
+    plt.loglog(fs[0:200*2],ps[0:200*2])
+    plt.ylabel('Power')
+    plt.xlabel('Frequency (Hz)')
+    plt.xlim([1, 150])
+    plt.title(title)
